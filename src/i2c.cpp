6c178fbcffe7dc45866fc6a4f58f6f481f265a0f
nickyblanch <70228627+nickyblanch@users.noreply.github.com>
2021-11-15 15:02:14 -0700

    I2C and timer

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -5,6 +5,7 @@
 #define wait_for_completion while(!(TWCR & (1 << TWINT)));
 
 
+
 void initI2C() {
   
   PRR0 &= ~(1<<PRTWI);  // wake up I2C module on AT2560 power management register
@@ -15,18 +16,30 @@ void initI2C() {
   TWBR = 0xC6; // bit rate generator = 10k  (TWBR = 198)
 
   TWCR |= (1 << TWINT )| (1 << TWEN); // enable two wire interface
+  // TWINT -> interrupt flag; flagged when reads or writes are completed
+  // TWEN -> turn I2C module on
 
 }
+
 
  
 void StartI2C_Trans(unsigned char SLA) {
-// this function initiates a start condition and calls slave device with SLA
+  // this function initiates a start condition and calls slave device with SLA
+  
   TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN); // clear TWINT, intiate a start condition and enable
+  // TWINT -> interrupt flag; flagged when reads or writes are completed
+  // TWSTA -> start condition bit
+  // TWEN -> turn I2C module on
+
   wait_for_completion;
+
   TWDR = (SLA << 1); // slave address + write bit '0'
+
   TWCR = (1<<TWINT)|(1<<TWEN);  // trigger action:clear flag and enable TWI
   wait_for_completion;
 }
+
+
 
 void StopI2C_Trans() {
   // this function sends a stop condition to stop I2C transmission
@@ -34,34 +47,48 @@ void StopI2C_Trans() {
   TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO); //trigger action:  send stop condition
 
 }
+
+
+
 void write(unsigned char data){
   // this function loads the data passed into the I2C data register and transmits
+
   TWDR = data; //load data into TWDR register
+  // This is the register address.
+
   TWCR = (1<<TWINT)|(1<<TWEN);  // trigger action:  clear flag and enable TWI
+  
   wait_for_completion;
 }
+
 
 
 void Read_from(unsigned char SLA, unsigned char MEMADDRESS){
   // this function sets up reading from SLA at the SLA MEMADDRESS 
 
-  StartI2C_Trans(SLA);
+  StartI2C_Trans(SLA); // device address
  
-  write(MEMADDRESS);
+  write(MEMADDRESS); // register address
   
   TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN); // restart to switch to read mode
+  // TWINT -> interrupt flag; flagged when reads or writes are completed
+  // TWSTA -> start condition bit
+  // TWEN -> turn I2C module on
+
   wait_for_completion;
+
   TWDR = (SLA << 1) | 0x01; // 7 bit address for slave plus read bit
   TWCR = (1 << TWINT) | (1 << TWEN)| (1 << TWEA);// trigger with master sending ack
   wait_for_completion;
   TWCR = (1<< TWINT) | (1 << TWEN);  // master can send a nack now
   wait_for_completion;
   TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO); // Stop condition
-// after this function is executed the TWDR register has the data from SLA that Master wants to read
+  // after this function is executed the TWDR register has the data from SLA that Master wants to read
 }
   
+
+
 unsigned char Read_data() // Returns the last byte  from the data register
 {
-
   return TWDR;
 }
--- src/timer.cpp
+++ src/timer.cpp
@@ -2,39 +2,80 @@
 
 void initTimer1(){
 
-// make timer for 1 second 
+    // Set waveform generation bits to CTC mode
+    // Check datasheet to see which bits to set to what
+    // According to datasheet: WGM13 = 0, WGM12 = 1, WGM11 = 0, WGM10 = 0 (for mode 4, CTC mode)
+    // Datasheet also shows the locations of these four bits
+    // Register TCCR1A: WGM10 and WGM11
+    // Register TCCR1B: WGM12 and WGM13
 
-//set waverform generations bits to CTC mode
-//WGM13=0; WGM12=1; WGM11=0; WGM10=0;
-TCCR1A&=~(1<<WGM10);
-TCCR1A&=~(1<<WGM11);
-TCCR1A|=(1<<WGM12);
-TCCR1B&=~(1<<WGM13);
-
-//OCR1A=1*40000000/1024=39062.5
-//set prescaler to 1024
-//CS12=1, CS11=0 CS10=1
-TCCR1B|=(1<<CS10)|(1<<CS12);
-TCCR1B&=~(1<<CS11);
-
-OCR1A=39062;
+    TCCR1A &= ~(1 << WGM10);    // Clear WGM10 bit in TCCR1A
+    TCCR1A &= ~(1 << WGM11);    // Clear WGM11 bit in TCCR1A
+    TCCR1B |=  (1 << WGM12);    // Set WGM11 bit in TCCR1B
+    TCCR1B &= ~(1 << WGM13);    // Clear WGM13 bit in TCCR1B
 
 }
 
 
 
-void delay_Ms(unsigned int delay){
+void delayMs(unsigned int delay){
+   // For the ms delay, I will use Timer 1 (the 16-bit timer)
 
-    unsigned int delayCnt= 0;TCNT0 = 0;
-    //starting the timer at 0 instead of some random junk number
-    TIFR0 |= (1 << OCF0A);// set compare flag to start timer
-    while (delayCnt< delay) {
-        if (TIFR0 & (1 << OCF0A)) {
-            //increment only while the flag is set (timer reached maxval= OCR0A)
-            delayCnt++;
-            TIFR0 |= (1 << OCF0A);
-            TCNT0 = 0;
-            //re-start timer. will go to 0 before reaching the if statement above
-        }
+    // this function takes an argument called delay which is the number of ms to delay
+    //
+    // the value of unsigned int delay can be any number that can fit within the scope of an unsigned int
+    // (because I am using software looping, there is no theoretical harware limitation to how high we can count, during the delay)
+    //
+
+    // CALCULATIONS:
+    // OCR1A = Td * Fclk / Ps - 1
+    // OCR1A = (1 * 10^-3) *  (16 * 10^6) / 64 - 1 = 249
+    // Prescaler: 64, OCR0A: 249
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // clear the TCNT1 registers (the counter registers)
+    TCNT1 = 0;
+    // TCNT1H = 0;
+    // TCNT1L = 0;
+    
+
+    // Set the values for OCR1A
+    // REMEMBER: OCR1A is an 16-bit register that is accessed over an 8-bit bus.
+    // Therefore, we can store a value up to 2^16-1 = 65535. In two different byte registers.
+    // Recall Count = (Td * frequency)/Prescaler = (1 ms * 16Mhz)/64 - 1 = 250 - 1 = 249
+    // OCR1A = 249; each ms delay requires us to count up to exactly 249
+    OCR1A = 249;
+
+
+    // Set the OCF1A overflow flag down (by writing a 1 to it - it's unintuitive but correct)
+    TIFR1 |= (1 << OCF1A);
+
+
+    // set the prescaler to 64 and start the timer
+    // CS12 = 0, CS11 = 1, CS10 = 1 for a prescaler of 1 (no prescaler)
+    // After the following lines run, the clock is ON
+    TCCR1B &= ~(1 << CS12); // Clear CS12
+    TCCR1B |= (1 << CS11);  // Set CS11
+    TCCR1B |= (1 << CS10);  // Set CS10
+
+
+    // The following loop uses a 1 ms base delay to create a multi-ms total delay
+    for (unsigned int i = 0; i < delay; i++) {
+
+        // Checking the output compare flag
+        while (!(TIFR1 & (1 << OCF1A))); // do nothing while flag is down
+        // NOTE: OCF1A, the flag bit, is stored in the TIFR1 register ????
+
+        // NOTE: When we reach this point in the code, the counter has reached the compare value (249) and the flag is triggered.
+
+        // Clear the OCF1A flag
+        TIFR1 |= (1 << OCF1A);
+
+        // clear the TCNT1 register (the counter register)
+        TCNT1 = 0;
     }
+
+    // Turn the timer off before exiting
+    TCCR1B &= ~(1 << CS11 | 1 << CS10);
 }
