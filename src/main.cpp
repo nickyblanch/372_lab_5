58f9209e0239f27dd9516bfc69990e52a84c2196
nickyblanch <70228627+nickyblanch@users.noreply.github.com>
2021-11-15 15:55:31 -0700

    main formatting

--- src/main.cpp
+++ src/main.cpp
@@ -23,32 +23,32 @@ typedef enum stateEnum {wait_press, debounce_press, wait_release, debounce_relea
 typedef enum LEDstate {smile, frown} LEDstate;
 // Initialize states. Remember to use volatile
 volatile stateEnum button_state = wait_press; // ASSUMING WE BEGIN IN A STATE WHERE THE BUTTON IS NOT BEING PRESSED
-volatile LEDstate AccelerationState=frown;//initialize the state to frown 
-//----------------------------------------------------------------------//
+volatile LEDstate AccelerationState=frown;    // Tnitialize the state to frown 
 
+//----------------------------------------------------------------------//
 // Main function
 int main(void) {
 
   // VARIABLES
-int freq_count = 500;
+  int freq_count = 500;
 
-Serial.begin(9600);
   // HARDWARE INITIALIZATIONS
-  initSwitchPD0();
+  initSwitchPD0();      // Initialize the switch
+  SPI_MASTER_Init();    //Initialize the SPI module
+  initTimer1();         // Initialize timer 1 (for millisecond delay)
+  Serial.begin(9600);   // Serial (for debugging)
+  sei();                // Enable global interrupts
 
-  
-  initTimer1();
-  sei();
-  SPI_MASTER_Init(); //Initialize the SPI module
+  // SETUP DISPLAY
+  write_execute(0x0A,0x08); //Brightness control
+  write_execute(0x0B,0x07); //scan all rows and collumns
+  write_execute(0x0C,0x01); //set shutdown register to normal operation
+  write_execute(0x0F,0x00); //display test register set to normal operation
+  AccelerationState=smile;  // Begin in the smile state
 
-  write_execute(0x0A,0x08);//Brightness control
-  write_execute(0x0B,0x07);//scan all rows and collumns
-  write_execute(0x0C,0x01);//set shutdown register to normal operation
-  write_execute(0x0F,0x00);//display test register set to normal operation
-   AccelerationState=smile;
-  while(1){
+  while(1) {
     
-    
+    // BUZZER:
     change_frequency(freq_count);
     freq_count++;
     if(freq_count > 10000){
@@ -58,37 +58,38 @@ Serial.begin(9600);
       freq_count = freq_count;
     }
 
+    // READ ACCELEROMETEROR
+    
 
-      delayMs(1000);
-      
+    // DISPLAY STATE MACHINE:      
     switch(AccelerationState){
         case smile:
-        OCR1C = 0;
-        write_execute(0x01,0b00000000);
-        write_execute(0x02,0b00000000);
-        write_execute(0x03,0b00100100);
-        write_execute(0x04,0b00000000);
-        write_execute(0x05,0b10000001);
-        write_execute(0x06,0b01100110);
-        write_execute(0x07,0b00111100);
-        write_execute(0x08,0b00000000);
-        AccelerationState=frown;
+          OCR1C = 0;
+          write_execute(0x01,0b00000000);
+          write_execute(0x02,0b00000000);
+          write_execute(0x03,0b00100100);
+          write_execute(0x04,0b00000000);
+          write_execute(0x05,0b10000001);
+          write_execute(0x06,0b01100110);
+          write_execute(0x07,0b00111100);
+          write_execute(0x08,0b00000000);
           break;
         case frown:
-        OCR1C = OCR1A * 0.25;
-        write_execute(0x01,0b00000000);
-        write_execute(0x02,0b00000000);
-        write_execute(0x03,0b00100100);
-        write_execute(0x04,0b00000000);
-        write_execute(0x05,0b00011000);
-        write_execute(0x06,0b01100110);
-        write_execute(0x07,0b10000001);
-        write_execute(0x08,0b00000000);
-        AccelerationState=smile;
-         break;
-    }
-    // Check the state
+          OCR1C = OCR1A * 0.25;
+          write_execute(0x01,0b00000000);
+          write_execute(0x02,0b00000000);
+          write_execute(0x03,0b00100100);
+          write_execute(0x04,0b00000000);
+          write_execute(0x05,0b00011000);
+          write_execute(0x06,0b01100110);
+          write_execute(0x07,0b10000001);
+          write_execute(0x08,0b00000000);
+          break;
+    } // end display state machine
+
+    // BUTTON STATE MACHINE:
     switch(button_state) {
+
       case wait_press:
         // Do nothing; we are waiting for the button to be pressed.
         break;
@@ -107,12 +108,12 @@ Serial.begin(9600);
         
         button_state = wait_press;
         break;
-    }
 
-    
-  }
+    } // end switch state machine  
+  } // end while loop
+} // end main
 
-}
+
 
 //----------------------------------------------------------------------//
 // Interrupt
@@ -127,9 +128,14 @@ ISR (INT0_vect) {
   else if (button_state == wait_release) {
     // The button was released while we were waiting for it to be released. Enter the second debounce state.
     button_state = debounce_release;
-    
-    // TODO
-    
-  }
+
+    // Buzzer:
+    if (OCR1C) {
+      OCR1C = 0;
+    }
+    else {
+      OCR1C = OCR1A*.25;
+    } // end if/else
+  } // end if/else
   // If the flag triggers while the button is 1 in one of the noisy debounce states, we do nothing.
-}
+} // end ISR
